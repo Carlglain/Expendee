6236a56f043d4d1d5a6c40d8cba1a4a26abe6ab4
Carlstorm <carlglainnyuysemo@gmail.com>
2025-05-04 00:53:46 +0100

    completed nav bar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from '../assets/image1Logo.png'
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import {Link} from 'react-router-dom'
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+
 function Navbar() {
+  const [open, setOpen] = useState(false)
+  const MenuCustom = ()=>{
+    return(
+      <div className='flex lg:hidden flex-col w-50 bg-gray-50 rounded-md p-4 fixed top-15 text-center right-0 justify-center items-center '>
+          <ul className='items-center space-y-2'>
+            < li className='mx-4 navurl'><Link>Features <ArrowDropDownIcon /></Link></li>
+            < li className='mx-4 navurl'><Link>Intergrations <ArrowDropDownIcon /></Link></li>
+            < li className='mx-4 navurl'><Link>Pricing</Link></li>
+            < li className='mx-4 navurl'><Link>Help Center</Link></li>
+            < li className='mx-4 navurl text-lg rounded-md bg-gradient-to-r px-3 form-orange-700 to-blue-500'><Link>Login</Link></li>
+            < li className='mx-4 text-lg text-white px-4 navurl bg-black rounded-md  '><Link>Start Free Trial</Link></li>
+          </ul>
+      </div>
+    )
+  }
   return (
     <nav className='top-0 sticky text-black w-full h-15 py-3 bg-gray-50'>
         <div className='container text-sm px-4 mx-auto relative'>
@@ -21,7 +39,12 @@ function Navbar() {
                 < li className='mx-4 navurl text-lg rounded-md bg-gradient-to-r px-3 form-orange-700 to-blue-500'><Link>Login</Link></li>
                 < li className='mx-4 text-lg text-white px-4 navurl bg-black rounded-md  '><Link>Start Free Trial</Link></li>
                 </ul>
-
+                <div className='lg:hidden md:flex justify-end flex-col text-black'>
+                <button  onClick={()=>{setOpen(open=>!open)}}>
+                  {open?<CloseIcon />: <MenuIcon />}
+                </button>
+                </div>
+                {open && <MenuCustom />}
             </div>
         </div>
     </nav>
