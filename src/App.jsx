2766d64283a3fa2674edb02ddb084c80f97e19b8
Carlstorm <carlglainnyuysemo@gmail.com>
2025-05-03 23:59:27 +0100

    working on responsive nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import Navbar from './Components/Navbar'
+import {Routes, Route} from 'react-router-dom'
 function App() {
   const [count , setCount] = useState(0)
   return (<div className='bg-black'>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,18 +1,27 @@
 import React from 'react'
 import Logo from '../assets/image1Logo.png'
-
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import {Link} from 'react-router-dom'
 function Navbar() {
   return (
-    <nav className='top-0 sticky text-white w-full h-15 py-3 bg-black'>
+    <nav className='top-0 sticky text-black w-full h-15 py-3 bg-gray-50'>
         <div className='container text-sm px-4 mx-auto relative'>
             <div className='flex items-center justify-between'>
                 <div className='flex items-center space-x-1'>
                     <img src={Logo} alt="Expendee logo" className='h-6 w-8' />
                     <span className='text-xl font-semibold font-poppins'>Expendee</span>
                 </div>
-                <div>
+                <ul className='hidden lg:flex items-center '>
+                  < li className='mx-4 navurl'><Link>Features <ArrowDropDownIcon /></Link></li>
+                  < li className='mx-4 navurl'><Link>Intergrations <ArrowDropDownIcon /></Link></li>
+                  < li className='mx-4 navurl'><Link>Pricing</Link></li>
+                  < li className='mx-4 navurl'><Link>Help Center</Link></li>
+                </ul>
+                <ul className='hidden lg:flex items-center'>
+                < li className='mx-4 navurl text-lg rounded-md bg-gradient-to-r px-3 form-orange-700 to-blue-500'><Link>Login</Link></li>
+                < li className='mx-4 text-lg text-white px-4 navurl bg-black rounded-md  '><Link>Start Free Trial</Link></li>
+                </ul>
 
-                </div>
             </div>
         </div>
     </nav>
