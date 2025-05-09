672e8d7d7ae495bea2bb958648e303245fad0413
Carlstorm <carlglainnyuysemo@gmail.com>
2025-05-09 21:07:51 +0100

    done for the day

--- src/Pages/home/HeroSection.jsx
+++ src/Pages/home/HeroSection.jsx
@@ -1,25 +1,32 @@
 import React from 'react'
 import WhereToVoteIcon from '@mui/icons-material/WhereToVote';
 import PlayCircleFilledIcon from '@mui/icons-material/PlayCircleFilled';
+import SyncLockIcon from '@mui/icons-material/SyncLock';
 import { Link } from 'react-router-dom';
 function HeroSection() {
   return (
-    <div className='text-lg'>
-    
-      <h1 className='font-bold text-[2rem] lg:text-[4rem] text-center mb-6'>Smart Expense Tracking Across All Your Mobile Money Platforms</h1>
-      <p className='lg:hidden'><WhereToVoteIcon />Gain complete visibility into your spending habits with our AI-powered analytics.
-   <br /><WhereToVoteIcon /> Connect all your accounts,
-   <br /><WhereToVoteIcon />automate categorization, 
-   <br /><WhereToVoteIcon />and make smarter financial decisions.</p>
-    
-    
-    <p className='hidden lg:block text-center text-xl'>Gain complete visibility into your spending habits with our AI-powered analytics. Connect all your accounts,automate categorization, and make
-smarter financial decisions.</p>
-<div className='my-4'>
-    <Link to='/registration'>Get Started for free</Link>
-<Link> <PlayCircleFilledIcon /> See how it works</Link>
-</div>
+    <div className='flex text-xl space-x-4'>
+        <div className=' w-1/2'>
+        
+            <h1 className='font-bold text-[2rem] lg:text-[4rem] mb-6'>Smart Expense Tracking Across All Your Mobile Money Platforms</h1>
+            <p className='lg:hidden'><WhereToVoteIcon />Gain complete visibility into your spending habits with our AI-powered analytics.
+            <br /><WhereToVoteIcon /> Connect all your accounts,
+            <br /><WhereToVoteIcon />automate categorization, 
+            <br /><WhereToVoteIcon />and make smarter financial decisions.</p>
+        
+        
+            <p className='hidden lg:block text-xl'>Gain complete visibility into your spending habits with our AI-powered analytics. Connect all your accounts,automate categorization, and make
+            smarter financial decisions.</p>
+        <div className='my-10 flex flex-col items-center lg:block '>
+            <span className='border rounded-md p-4 text-center my-2 mr-6 text-white bg-gray-900'> <Link to='/registration'>Get Started for free</Link> </span>
+            <Link> <PlayCircleFilledIcon /> See how it works</Link>
+        </div>
+        <p><SyncLockIcon /> Bank-level security with 256-bit encryption </p>
     </div>
+    <div className='w-1/2 flex items-center justify-center'>
+        <span className='bg-white p-8'> <Link className='bg-gray-900 text-white p-4 rounded-md shadow'>Dashboard visualization showing expense analytics and transaction data</Link> </span>
+    </div>
+</div>
   )
 }
 
--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -6,6 +6,16 @@ function Home() {
   return (
     <div className='bg-gray-50 h-screen py-6 px-6'>
       <HeroSection />
+      <div className='bg-white h-50 my-10'>
+        <h2>Trusted by users across these platforms</h2>
+        <div>
+          <p>MTN MOMO</p>
+          <p>ORANGE MONEY</p>
+          <p>Paypal</p>
+          <p>Binance</p>
+        </div>
+      </div>
+
     </div>
   )
 }
