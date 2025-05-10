beff0b0ca3cc82f4ea2900ea3d01d2d2db47c97e
Carlstorm <carlglainnyuysemo@gmail.com>
2025-05-11 00:12:18 +0100

    the more streak the better

---
+++ src/Pages/home/FeaturesSection.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function FeaturesSection() {
+  return (
+    <div>
+      <div className='text-center text-xl my-12'>
+       <h1 className='text-[2rem] font-bold '> Powerful Features for Complete Financial Control</h1>
+       <p>Our platform combines intelligent analytics with intuitive design 
+          to give you <br /> unprecedented insight into your spending habits.</p>
+      </div>
+      {/* card */}
+      <div className='flex justify-center items-center'>
+        
+      </div>
+    </div>
+  )
+}
+
+export default FeaturesSection
--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -4,12 +4,13 @@ import HeroSection from './HeroSection'
 import SendToMobileIcon from '@mui/icons-material/SendToMobile';
 import PaymentsIcon from '@mui/icons-material/Payments';
 import RequestQuoteIcon from '@mui/icons-material/RequestQuote';
+import FeaturesSection from './FeaturesSection';
 function Home() {
   return (
     <div className='bg-gray-50 h-screen py-6 px-6'>
       <HeroSection />
-      <div className='bg-white h-50 my-10'>
-        <h2 className='text-center pt'>Trusted by users across these platforms</h2>
+      <div className='bg-white h-40 my-10'>
+        <p className='text-center pt-4 tex-xl '>Trusted by users across these platforms</p>
         <div className='flex justify-around items-center mt-6'>
           <p><SendToMobileIcon /> MTN MOMO</p>
           <p><PaymentsIcon /> ORANGE MONEY</p>
@@ -17,6 +18,7 @@ function Home() {
           <p><RequestQuoteIcon /> Binance</p>
         </div>
       </div>
+      <FeaturesSection />
 
     </div>
   )
